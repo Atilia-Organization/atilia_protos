5a23a7d4efef9339df2b7f612ee22b8e48cd5236
Haliyong <neoaurellano@gmail.com>
2025-05-26 13:57:08 +0800

    Animation after submission implemented

--- webapp/src/Landing.jsx
+++ webapp/src/Landing.jsx
@@ -16,10 +16,14 @@ function Landing() {
     }
   };
 
+  {/* for animation */}
+  const [showMailAnimation, setShowMailAnimation] = useState(false);
+
   const handleNameSubmit = () => {
     if (name) {
       console.log("Name submitted: ", name);
       console.log("Email: ", email);
+
 
       fetch(import.meta.env.VITE_ADD_EMAIL_TO_SHEETS, {
         method: "POST",
@@ -27,6 +31,7 @@ function Landing() {
         body: (`Name=${name}&Email=${email}`)
       }).then(res=>res.text()).then(data=>{
         alert(data)
+        setShowMailAnimation(true); // Trigger animation here
       }).catch(error=>console.log(error))
     }
   };
@@ -61,54 +66,13 @@ function Landing() {
 
         {/* Hero */}
         <div className="flex-1 flex items-center justify-center">
-          <div className="hero">
-            <div className="hero-content text-left">
-              <div className="max-w-xl p-5 md:p-3">
-                <div className="overflow-hidden">
-                  <div
-                    className={`flex transition delay-150 duration-300 ease-in-out`}
-                    style={{
-                      transform: `translateX(${step === 1 ? "0%" : "-50%"})`,
-                      width: "200%",
-                    }}
-                  >
-                    <div>
-                      <p className="text-3xl md:text-5xl font-bold ">
-                        <span className="text-purple-primary">Growth</span>{" "}
-                        Isn’t an option,{" "}
-                        <span className="text-purple-primary">
-                          It’s the mission.
-                        </span>
-                      </p>
-                      <p className="py-6 text:lg md:font-semibold md:text-xl">
-                        Get exclusive early access to updates. Don’t miss your
-                        chance to be part of something transformative.
-                      </p>
-                    </div>
-
-                    <div>
-                      <p className="text-3xl md:text-5xl font-bold ">
-                        Let’s get to know you
-                        <br />—
-                        <span className="text-purple-primary">
-                          what’s your name?
-                        </span>
-                      </p>
-                      <p className="py-6 text:lg md:font-semibold md:text-xl">
-                        In a world full of data, we’re choosing connection. We
-                        want to know your name—not just your inbox.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    if (!e.currentTarget.checkValidity()) return;
-                    step === 1 ? handleEmailSubmit() : handleNameSubmit();
-                  }}
-                >
-                  <fieldset className="fieldset overflow-hidden">
+           {showMailAnimation ? (
+              <div className="mail-animation"></div>
+            ) : (
+            <div className="hero">
+              <div className="hero-content text-left">
+                <div className="max-w-xl p-5 md:p-3">
+                  <div className="overflow-hidden">
                     <div
                       className={`flex transition delay-150 duration-300 ease-in-out`}
                       style={{
@@ -116,56 +80,101 @@ function Landing() {
                         width: "200%",
                       }}
                     >
-                      <HeroInput subscribeText="Subscribe" optional={true}>
-                        <label className="input validator w-full bg-white join-item">
-                          <svg
-                            className="h-[1em] opacity-50 text-gray-700"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                          >
-                            <g
-                              strokeLinejoin="round"
-                              strokeLinecap="round"
-                              strokeWidth="2.5"
-                              fill="none"
-                              stroke="currentColor"
-                            >
-                              <rect
-                                width="20"
-                                height="16"
-                                x="2"
-                                y="4"
-                                rx="2"
-                              ></rect>
-                              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                            </g>
-                          </svg>
-                          <input
-                            className="text-gray-700"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            placeholder="mail@gmail.com"
-                            required
-                          />
-                        </label>
-                      </HeroInput>
+                      <div>
+                        <p className="text-3xl md:text-5xl font-bold ">
+                          <span className="text-purple-primary">Growth</span>{" "}
+                          Isn’t an option,{" "}
+                          <span className="text-purple-primary">
+                            It’s the mission.
+                          </span>
+                        </p>
+                        <p className="py-6 text:lg md:font-semibold md:text-xl">
+                          Get exclusive early access to updates. Don’t miss your
+                          chance to be part of something transformative.
+                        </p>
+                      </div>
 
-                      <HeroInput subscribeText="Enter">
-                        <input
-                          type="text"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          placeholder="Enter your Name"
-                          className="input rounded-lg w-full text-gray-700 bg-white join-item"
-                        />
-                      </HeroInput>
+                      <div>
+                        <p className="text-3xl md:text-5xl font-bold ">
+                          Let’s get to know you
+                          <br />—
+                          <span className="text-purple-primary">
+                            what’s your name?
+                          </span>
+                        </p>
+                        <p className="py-6 text:lg md:font-semibold md:text-xl">
+                          In a world full of data, we’re choosing connection. We
+                          want to know your name—not just your inbox.
+                        </p>
+                      </div>
                     </div>
-                  </fieldset>
-                </form>
+                  </div>
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      if (!e.currentTarget.checkValidity()) return;
+                      step === 1 ? handleEmailSubmit() : handleNameSubmit();
+                    }}
+                  >
+                    <fieldset className="fieldset overflow-hidden">
+                      <div
+                        className={`flex transition delay-150 duration-300 ease-in-out`}
+                        style={{
+                          transform: `translateX(${step === 1 ? "0%" : "-50%"})`,
+                          width: "200%",
+                        }}
+                      >
+                        <HeroInput subscribeText="Subscribe" optional={true}>
+                          <label className="input validator w-full bg-white join-item">
+                            <svg
+                              className="h-[1em] opacity-50 text-gray-700"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                            >
+                              <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                              >
+                                <rect
+                                  width="20"
+                                  height="16"
+                                  x="2"
+                                  y="4"
+                                  rx="2"
+                                ></rect>
+                                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                              </g>
+                            </svg>
+                            <input
+                              className="text-gray-700"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                              type="email"
+                              placeholder="mail@gmail.com"
+                              required
+                            />
+                          </label>
+                        </HeroInput>
+
+                        <HeroInput subscribeText="Enter">
+                          <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Enter your Name"
+                            className="input rounded-lg w-full text-gray-700 bg-white join-item"
+                          />
+                        </HeroInput>
+                      </div>
+                    </fieldset>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
+            )}
         </div>
 
         {/* Footer */}
